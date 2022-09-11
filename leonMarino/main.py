32baff355c188c2607be1e3ca8c7f81489434b91
[Pedro Scheeffer] <[pedrosd26@gmail.com]>
2022-09-11 17:38:58 -0300

    TODO

--- leonMarino/main.py
+++ leonMarino/main.py
@@ -3,3 +3,6 @@ import numpy as np #https://numpy.org/doc/stable/reference/index.html
 import matplotlib.pyplot as plt #https://matplotlib.org/stable/index.html
 import geopandas as gpd 
 
+def styleLines(dataset):
+    #TODO: codigo de secuencia de collatz, quedo bueno 
+    return dataset.plot()

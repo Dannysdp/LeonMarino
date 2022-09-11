fff3e078a253f365ee43a03660fbb79e6caf03d3
[Pedro Scheeffer] <[pedrosd26@gmail.com]>
2022-09-11 12:38:12 -0300

    Init

---
+++ leonMarino/main.py
@@ -0,0 +1,5 @@
+import pandas as pd
+import numpy as np #https://numpy.org/doc/stable/reference/index.html
+import matplotlib.pyplot as plt #https://matplotlib.org/stable/index.html
+import geopandas as gpd 
+

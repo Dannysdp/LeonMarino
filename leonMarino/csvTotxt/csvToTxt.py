d66baae91063afcc705314b6718e14c96daad676
[Pedro Scheeffer] <[pedrosd26@gmail.com]>
2022-09-24 13:06:50 -0300

    lector implementado, ahora crear clase paper y formateo correcto

--- leonMarino/csvTotxt/csvToTxt.py
+++ leonMarino/csvTotxt/csvToTxt.py
@@ -10,10 +10,19 @@ def main():
     #path = path[1:len(path)-1]
     path = "D:\Drive\Analisis Trabajos - Copy.csv"
     #rows = int(input("Number of rows"))
-    rows = 12
     with open(path,"r",newline='',encoding="utf-8") as csvfile:
-        pd.open
-        paper = paper
-        for row in paper:
+        papers = csv.reader(csvfile,dialect="excel")
+        for row in papers:
             print(row)
+class paper:
+    def __init__(self,doi,title,descr,authors,tags):
+        self.doi = doi
+        self.title = title
+        self.descr = descr
+        self.authors = authors
+        self.tags = tags
+    
+    def setAutors(self,authors):  #TODO: implementar formateo correcot #nombre_apellido" ", 
+         authors = name_surname
+    
 main()

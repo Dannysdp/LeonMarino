9776f399efb5efaef5488a0c66ca1b6725fef681
[Pedro Scheeffer] <[pedrosd26@gmail.com]>
2022-09-24 11:43:26 -0300

    changing from base csv reader to padnascsv

--- leonMarino/csvTotxt/csvToTxt.py
+++ leonMarino/csvTotxt/csvToTxt.py
@@ -1,1 +1,19 @@
- 
+from ast import Num
+from codecs import charmap_build
+import csv
+import numpy as np
+import pandas as pd
+
+def main():
+    print("Bienvenido al conversor")
+    #path = str(input("File path for reading, remember to have the right formating \n:"))
+    #path = path[1:len(path)-1]
+    path = "D:\Drive\Analisis Trabajos - Copy.csv"
+    #rows = int(input("Number of rows"))
+    rows = 12
+    with open(path,"r",newline='',encoding="utf-8") as csvfile:
+        pd.open
+        paper = paper
+        for row in paper:
+            print(row)
+main()

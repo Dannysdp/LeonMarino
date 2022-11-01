c65c4e12b4b2c77231e48b40cc3e011e38b4b5c2
[Pedro Scheeffer] <pedro.s.d.p@hotmail.com>
2022-10-31 23:18:00 -0300

    Description

--- leonMarino/csvTotxt/csvToTxt.py
+++ leonMarino/csvTotxt/csvToTxt.py
@@ -7,8 +7,9 @@ def main():
 
 #TODO: separa logica
 def csv2paper(path):
-    '''Gets a csv file with papers with that 
-    has the same order of col that the class Paper'''   
+    '''Gets a csv file with papers that 
+    has the same order of col that the class Paper
+    and prints the markdown file in a folder'''   
     with open(path, "r", newline='', encoding="utf-8") as csvfile:
         papers = csv.reader(csvfile, dialect="excel")
         for row in papers:
@@ -16,5 +17,4 @@ def csv2paper(path):
                 col = col.strip()
             newPaper = paper(row[0],row[1],row[2],row[3],row[4])
             paper.writeFile(newPaper)
-
 main()
